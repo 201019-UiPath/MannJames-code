68fef773ad4dca10ec224f887599756acdf4e9f1
mannj2015 <72755084+mannj2015@users.noreply.github.com>
2020-10-20 10:24:16 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//dotnet new console -o helloWorld
+//1.0 prefefined namespace
+using System;
+
+//1.1 custom defined namespace
+namespace helloWorld
+{
+    class Program
+    {
+        //start execution
+            //commandline arguments
+        static void Main(string[] args)
+        {
+            string fname, lname;
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine("please enter fname");
+            fname = Console.ReadLine();
+            Console.WriteLine("please enter lname");
+            lname = Console.ReadLine();
+            //interpolation
+            Console.WriteLine($"welcome {fname} {lname} !");
+
+            //add date time
+            //sql datetime?
+
+            //adding weather?
+
+            //connocation - less efficient
+            //System.Console.WriteLine("w " + fname + " " + lname);
+        }
+    }
+}
