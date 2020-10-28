a934127bcc87242d32eb8679b7fee8d37a509bc0
mannj2015 <72755084+mannj2015@users.noreply.github.com>
2020-10-28 18:31:12 -0400

    all except entity list is done with librarycontext

--- MannJames-Project0/proj0/Libraries/LibraryContext.cs
+++ MannJames-Project0/proj0/Libraries/LibraryContext.cs
@@ -16,5 +16,29 @@ namespace Libraries
         public DbSet<Location> Location { get; set; }
         public DbSet<Order> Order { get; set; }
 
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+/*
+        modelBuilder.Entity<Publisher>(entity =>
+      {
+        entity.HasKey(e => e.ID);
+        entity.Property(e => e.Name).IsRequired();
+    });
+
+      modelBuilder.Entity<Book>(entity =>
+      {
+        entity.HasKey(e => e.ISBN);
+        entity.Property(e => e.Title).IsRequired();
+    entity.HasOne(d => d.Publisher)
+          .WithMany(p => p.Books);
+});
+*/
     }
 }
