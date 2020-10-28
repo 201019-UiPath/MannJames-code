013e7e1b6e7a4989c837c182d04e7edf18198ccf
mannj2015 <72755084+mannj2015@users.noreply.github.com>
2020-10-28 16:16:57 -0400

    Wrote part of LibraryContext

---
+++ MannJames-Project0/proj0/Libraries/LibraryContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Libraries
+{
+    public class LibraryContext: DbContext
+    {
+        public DbSet<CardPayment> Card { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<InvProduct> InvProject { get; set; }
+        public DbSet<OrdProduct> OrdProject { get; set; }
+        public DbSet<Location> Location { get; set; }
+        public DbSet<Order> Order { get; set; }
+
+    }
+}
