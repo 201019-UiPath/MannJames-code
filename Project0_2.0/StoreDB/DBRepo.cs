d7dce6962cd633ed91cb63289dd35633e3f84f2e
mannj2015 <72755084+mannj2015@users.noreply.github.com>
2020-11-05 20:31:13 -0500

    all repository code finished. needs unit tests

--- Project0_2.0/StoreDB/DBRepo.cs
+++ Project0_2.0/StoreDB/DBRepo.cs
@@ -1,12 +1,13 @@
 ﻿using StoreDB.Models;
 using StoreDB.Repos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace StoreDB
 {
     public class DBRepo : IProductRepo, ILocationRepo, IInventoryItemRepo, 
-        ICartItemRepo, ICartRepo
+        ICartItemRepo, ICartRepo, IUserRepo, ILineItemRepo, IOrderRepo
     {
         private StoreContext context;
         public DBRepo(StoreContext context)
@@ -219,6 +220,150 @@ namespace StoreDB
         {
             context.Carts.Remove(cart);
             context.SaveChanges();
+        }
+        /// <summary>
+        /// User methods run
+        /// </summary>
+        /// <param name="user"></param>
+        public void AddUser(User user)
+        {
+            context.Users.Add(user);
+            context.SaveChanges();
+        }
+
+        public void UpdateUser(User user)
+        {
+            context.Users.Update(user);
+            context.SaveChanges();
+        }
+
+        public User GetUserById(int id)
+        {
+            return context.Users.Single(q => q.UserId == id);
+        }
+
+        public User GetUserByUsername(string username)
+        {
+            return context.Users.Single(q => q.Username==username);
+        }
+
+        public List<User> GetAllUsers()
+        {
+            return context.Users.Select(q => q).ToList();
+        }
+
+        public void DeleteUser(User user)
+        {
+            context.Users.Remove(user);
+            context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Line Item methods
+        /// </summary>
+        /// <param name="lineItem"></param>
+
+        public void AddLineItem(LineItem lineItem)
+        {
+            context.LineItems.Add(lineItem);
+            context.SaveChanges();
+        }
+
+        public void UpdateLineItem(LineItem lineItem)
+        {
+            context.LineItems.Update(lineItem);
+            context.SaveChanges();
+        }
+
+        public LineItem GetLineItemByOrderId(int id)
+        {
+            return context.LineItems.Single(q => q.OrderId == id);
+        }
+
+        public List<LineItem> GetAllLineItemsByOrderId(int id)
+        {
+            return context.LineItems.Where(q => q.OrderId == id).ToList();
+        }
+
+        public void DeleteLineItem(LineItem lineItem)
+        {
+            context.LineItems.Remove(lineItem);
+            context.SaveChanges();
+        }
+        /// <summary>
+        /// order methods (including sorting by date and cost)
+        /// </summary>
+        /// <param name="order"></param>
+        public void AddOrder(Order order)
+        {
+            context.Orders.Add(order);
+            context.SaveChanges();
+        }
+
+        public void UpdateOrder(Order order)
+        {
+            context.Orders.Update(order);
+            context.SaveChanges();
+        }
+
+        public Order GetOrderById(int id)
+        {
+            return context.Orders.Single(q => q.OrderId ==id);
+        }
+
+        public Order GetOrderByUserId(int id)
+        {
+            return context.Orders.Single(q => q.UserId == id);
+        }
+
+        public Order GetOrderByLocationId(int id)
+        {
+            return context.Orders.Single(q => q.LocationId == id);
+        }
+
+        public List<Order> GetAllOrdersByLocationId(int id)
+        {
+            return context.Orders.Where(q => q.LocationId == id).ToList();
+        }
+
+        public List<Order> GetAllOrdersByUserId(int id)
+        {
+            return context.Orders.Where(q => q.UserId == id).ToList();
+        }
+
+        public void DeleteOrder(Order order)
+        {
+            context.Orders.Remove(order);
+            context.SaveChanges();
+        }
+
+        public List<Order> GetAllOrdersByUserIdDateAsc(int id)
+        {
+            return context.Orders.Where(x => x.UserId == id)
+                .OrderBy(x => x.OrderDate).ToList();
+        }
+
+        public List<Order> GetAllOrdersByUserIdDateDesc(int id)
+        {
+            return context.Orders.Where(x => x.UserId == id)
+                .OrderByDescending(x => x.OrderDate).ToList();
+        }
+
+        public List<Order> GetAllOrdersByUserIdPriceAsc(int id)
+        {
+            return context.Orders.Where(x => x.UserId == id)
+                .OrderBy(x => x.TotalPrice).ToList();
+        }
+
+        public List<Order> GetAllOrdersByUserIdPriceDesc(int id)
+        {
+            return context.Orders.Where(x => x.UserId == id)
+                .OrderByDescending(x => x.TotalPrice).ToList();
+        }
+
+        public Order GetOrderByDate(DateTime dateTime)
+        {
+            return context.Orders.Single(q => q.OrderDate == dateTime);
         }
     }
 }
