a6d828fb8fd313028bee27709fe16de78925a28d
mannj2015 <72755084+mannj2015@users.noreply.github.com>
2020-11-09 19:51:56 -0500

    created project 1 and created half of the IServices and some of ASP project

---
+++ project1/StoreAPI/Controllers/ProductController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StoreDB.Models;
+using StoreLib;
+
+namespace StoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService productService;
+        public ProductController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+        [HttpPost("add")]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        public IActionResult AddProduct(Product product)
+        {
+            try
+            {
+                productService.AddProduct(product);
+                return CreatedAtAction("AddProduct", product);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut("edit")]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        public IActionResult UpdateProduct(Product product)
+        {
+            try
+            {
+                productService.UpdateProduct(product);
+                return CreatedAtAction("UpdateProduct", product);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("delete")]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        public IActionResult DeleteProduct(Product product)
+        {
+            try
+            {
+                productService.DeleteProduct(product);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("get")]
+        [Produces("application/json")]
+        public IActionResult GetAllProducts()
+        {
+            try
+            {
+                return Ok(productService.GetAllProducts());
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpGet("get/{productName}")]
+        [Produces("application/json")]
+        public IActionResult GetProductByName(string productName)
+        {
+            try
+            {
+                //TODO will need to adjust this to search w/o case sensitivity
+                return Ok(productService.GetProductByName(productName));
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+        }
+
+
+
+        //TODO this should be removed as it is not used List<Product> GetAllProductsAtLocationId(int id);
+        // public IActionResult GetProductById(int id) { }
+    }
+}
---
+++ project1/StoreLib/ILocationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StoreDB.Models;
+
+namespace StoreLib
+{
+    public interface ILocationService
+    {
+    }
+}
