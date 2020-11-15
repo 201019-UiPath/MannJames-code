bb3d5509b4172df235887d1963cfb4429005cc3f
mannj2015 <72755084+mannj2015@users.noreply.github.com>
2020-11-15 00:27:09 -0500

    Update HomeController.cs

--- project1/StoreWeb/Controllers/HomeController.cs
+++ project1/StoreWeb/Controllers/HomeController.cs
@@ -8,12 +8,13 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Diagnostics;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace StoreWeb.Controllers
 {
     public class HomeController : Controller
     {
-
+        readonly HttpClient client = new HttpClient();
         private const string url = "https://localhost:44317/";
         private readonly ILogger<HomeController> _logger;
 
@@ -22,130 +23,39 @@ namespace StoreWeb.Controllers
             _logger = logger;
         }
         [HttpGet]
-        public ViewResult Login()
+        public async Task<ViewResult> Login()
         {
             var model = new LoginModel();
+            //await model;
             return View(model);
         }
         [HttpPost]
-        public IActionResult Login(LoginModel model)
+        public async Task<IActionResult> Login(LoginModel model)
         {
-            if (ModelState.IsValid)
+            HttpClientHandler handler = new HttpClientHandler();
+            var client = new HttpClient(handler);
+            client.BaseAddress = new Uri(url);
+            var response = client.GetAsync($"user/get/name={model.Username}");
+            await response;
+            //problem is here in debug
+            if (response.Result.IsSuccessStatusCode)
             {
-                using (var client = new HttpClient())
+                var jsonString = response.Result.Content.ReadAsStringAsync();
+                await jsonString;
+                var verifiedUser = JsonConvert.DeserializeObject<User>(jsonString.Result);
+                if (verifiedUser.Password == model.Password && verifiedUser.Username == model.Username)
                 {
-                    client.BaseAddress = new Uri(url);
-                    var response = client.GetAsync($"user/get?name={model.Username}");
-                    //HttpRequestException: 
-                    //No connection could be made because the target machine actively refused it.
-
-                    //SocketException: 
-                    //No connection could be made because the target machine actively refused it.
-
-                    //https://aspnetmonsters.com/2016/08/2016-08-27-httpclientwrong/
-
-                    response.Wait();
-
-                    var result = response.Result;
-                    if (result.IsSuccessStatusCode)
-                    {
-                        var jsonString = result.Content.ReadAsStringAsync();
-                        jsonString.Wait();
-
-                        var verifiedUser = JsonConvert.DeserializeObject<User>(jsonString.Result);
-
-                        if (verifiedUser.Password == model.Password && verifiedUser.Username == model.Username)
-                        {
-                            HttpContext.Session.SetObject("User", verifiedUser);
-                            return RedirectToAction("GetInventory", "Customer");
-                        }
-                        else
-                        {
-                            ModelState.AddModelError("Error", "Invalid information");
-                            return View(model);
-                        }
-                    }
-                }
-            }
-            return View(model);
-        }
-        [HttpPost]
-        public IActionResult SignUp(SignUpModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                if (model.Password == model.ConfirmPassword)
-                {
-                    User newUser = new User();
-                    newUser.Username = model.Username;
-                    newUser.Email = model.Email;
-                    newUser.LocationId = model.LocationId;
-                    newUser.Type = Models.User.UserType.Customer;
-                    newUser.Password = model.Password;
-
-                    using (var client = new HttpClient())
-                    {
-                        client.BaseAddress = new Uri(url);
-
-                        var json = JsonConvert.SerializeObject(newUser);
-                        var data = new StringContent(json, Encoding.UTF8, "application/json");
-
-                        var response = client.PostAsync("user/add", data);
-                        response.Wait();
-
-                        var result = response.Result;
-                        if (result.IsSuccessStatusCode)
-                        {
-                            HttpContext.Session.SetObject("User", newUser);
-                            return RedirectToAction("GetInventory", "Customer");
-                        }
-                    }
+                    HttpContext.Session.SetObject("User", verifiedUser);
+                    return RedirectToAction("GetInventory", "Customer");
                 }
                 else
                 {
-                    // TODO Failed sign in
+                    ModelState.AddModelError("Error", "Invalid information");
+                    return View(model);
                 }
             }
             return View(model);
         }
-        [HttpGet]
-        public ViewResult SignUp()
-        {
-            var model = new SignUpModel();
 
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri(url);
-                var response = client.GetAsync("location/getAll");
-                response.Wait();
-                var result = response.Result;
-                if (result.IsSuccessStatusCode)
-                {
-                    var jsonString = result.Content.ReadAsStringAsync();
-                    jsonString.Wait();
-                    var locations = JsonConvert.DeserializeObject<List<Location>>(jsonString.Result);
-                    var locationOptions = new List<SelectListItem>();
-                    foreach (var loc in locations)
-                    {
-                        locationOptions.Add(new SelectListItem 
-                        { Selected = false, Text = $"{loc.City}, {loc.State}", Value = loc.LocationId.ToString() });
-                    }
-                    model.LocationOptions = locationOptions;
-                }
-            }
-
-            return View(model);
-        }
-        public IActionResult SignOut()
-        {
-            HttpContext.Session.Clear();
-
-            return View("Login");
-        }
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
